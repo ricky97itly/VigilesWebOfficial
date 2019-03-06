f882a09d447189a06747aabeda4220fb1fe828e3
AlePalu <alessandro.palumbo@ied.edu>
2019-03-06 13:45:10 +0100

    Primo commit di Ale

--- vigilesweb/resources/views/welcome.blade.php
+++ vigilesweb/resources/views/welcome.blade.php
@@ -7,7 +7,7 @@
         <title>Laravel</title>
 
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
         <!-- Styles -->
         <style>
@@ -81,15 +81,17 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    VIGILES
+                    Laravel
                 </div>
 
                 <div class="links">
-                    <p>CIAO, SONO ALE! :D</>
-                    <p>Mettete</a>
-                    <p>Qua</p>
-                    <p>I Vostri</p>
-                    <p>Nomi</p>
+                    <a href="https://laravel.com/docs">Docs</a>
+                    <a href="https://laracasts.com">Laracasts</a>
+                    <a href="https://laravel-news.com">News</a>
+                    <a href="https://blog.laravel.com">Blog</a>
+                    <a href="https://nova.laravel.com">Nova</a>
+                    <a href="https://forge.laravel.com">Forge</a>
+                    <a href="https://github.com/laravel/laravel">GitHub</a>
                 </div>
             </div>
         </div>
