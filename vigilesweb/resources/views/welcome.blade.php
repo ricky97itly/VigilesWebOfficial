4b8eddea4e12aa12b6ef36daf7650a1e2ae9d46d
Riccardo Mores <riccardo.mores@ied.edu>
2019-03-06 15:33:01 +0100

    Riccardo
    Onnipresente

--- vigilesweb/resources/views/welcome.blade.php
+++ vigilesweb/resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    RICCARDO è STATO QUI
                 </div>
 
                 <div class="links">
