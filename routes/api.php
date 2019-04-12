1c588396c76c2543de68cc779fde457b5378dd7c
Riccardo Mores <riccardo.mores@ied.edu>
2019-04-12 16:52:36 +0200

    API iniziate
    Abbiamo creato un nuovo controller (ReportsAPIController) che viene richiamato dall’api.php.
    Ci sono i commenti per aiutare e provando a fare una get request da Postman all’url http://vigilesweb.test/api/reports possiamo vedere le nostre segnalazioni in formato JSON.
    Mi manca iOS.

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,18 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Mostra Segnalazioni
+Route::get('reports', 'ReportsAPIController@index');
+
+// Mostra segnalazione singola
+Route::get('report/{id}', 'ReportsAPIController@show');
+
+// Crea nuova segnalazione
+Route::post('report', 'ReportsAPIController@store');
+
+// Aggiorna segnalazione
+Route::put('report', 'ReportsAPIController@store');
+
+// Cancella segnalazione
+Route::delete('report/id', 'ReportsAPIController@destroy');
