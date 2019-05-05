c0f42b35b01e439c51e4dd49157a9152083ff925
Riccardo Mores <riccardo.mores@ied.edu>
2019-05-05 16:59:37 +0200

    Oauth funzionante
    In postman fare chiamata in post http://vigilesweb.test/oauth/token.
    Nel body inserire questi campi:
    grant_type -  password
    client_secret - la trovate nella tabella oauth clients
    client_id - scegliete dalla tabella
    username - inserite username
    password - scrivete password
    
    Fare poi chiamata in get http://vigilesweb.test/api/user e dentro header inserire:
    
    Accept - application/json
    Authorization - Bearer e numero access token visibile nella chiamata post

--- public/js/app.js
+++ public/js/app.js
@@ -52176,8 +52176,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/masterp/Desktop/VIGILESWEB/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/masterp/Desktop/VIGILESWEB/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/riccardomores/Desktop/VigilesWeb/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/riccardomores/Desktop/VigilesWeb/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
